75d146e7902caebcd8833ba0fc099fac3481d5fe
mlz <2692841607@qq.com>
2020-11-17 09:02:48 +0800

    取消打印

--- src/main/java/mlz/controller/HomeController.java
+++ src/main/java/mlz/controller/HomeController.java
@@ -9,7 +9,6 @@ public class HomeController {
 
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public String Home(){
-        System.out.println("打印");
         return "/home";
     }
 }
